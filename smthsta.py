625d5f10855df16644ba7aed0f7c169ddf32ec5a
liujian <liujian3@gmail.com>
2012-05-15 19:58:25 +0800

    urllib Signed-off-by: liujian <liujian3@gmail.com>

--- smthsta.py
+++ smthsta.py
@@ -1,1 +1,19 @@
-print("asdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfaasdfasdfasdfasdfadfasdfasdfa")
+import urllib.request
+import sys
+ 
+#cookie = http.cookiejar.CookieJar()                                        
+#cjhdr  =  urllib.request.HTTPCookieProcessor(cookie)
+#opener = urllib.request.build_opener(cjhdr)
+ 
+url = "http://www.newsmth.net/nForum/#!board/GOLD"
+postdata = urllib.parse.urlencode({'email': '112@gmail.com', 'password': '111123', 'Submit':''})
+postdata = postdata.encode('utf-8')
+ 
+res = urllib.request.urlopen(url,postdata)
+f=open("1.txt",'w')
+f.write(res.read().decode('gbk'))
+ 
+if( res.status != 200 ):
+    exit()
+ 
+print('ok')
