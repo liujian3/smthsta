939eabd7f0b39bddee54adaac65fcb50022060fd
liujian <liujian3@gmail.com>
2012-05-17 19:37:32 +0800

    Signed-off-by: liujian <liujian3@gmail.com>

--- smthsta.py
+++ smthsta.py
@@ -1,19 +1,19 @@
+#coding=gb2312
 import urllib.request
 import sys
- 
+import re
+
 #cookie = http.cookiejar.CookieJar()                                        
 #cjhdr  =  urllib.request.HTTPCookieProcessor(cookie)
 #opener = urllib.request.build_opener(cjhdr)
  
-url = "http://www.newsmth.net/nForum/#!board/GOLD"
-postdata = urllib.parse.urlencode({'email': '112@gmail.com', 'password': '111123', 'Submit':''})
-postdata = postdata.encode('utf-8')
+url = "http://www.newsmth.net/bbsdoc.php?board=GOLD"
+#postdata = urllib.parse.urlencode({'email': '112@gmail.com', 'password': '111123', 'Submit':''})
+#postdata = postdata.encode('utf-8')
  
-res = urllib.request.urlopen(url,postdata)
-f=open("1.txt",'w')
-f.write(res.read().decode('gbk'))
- 
-if( res.status != 200 ):
-    exit()
- 
-print('ok')
+#res = urllib.request.urlopen(url)
+#resstr = str(res.read().decode('gb2312'))
+#print(resstr)
+pat = re.compile(r'[\u4e00-\u9fa5] ')
+mat = pat.match(r'\xc8\xcb')
+print(mat)
